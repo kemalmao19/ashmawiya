aecf8b5e4e8d7a4c978b45cf6f350c7e1a36fe46
kemalmao19 <sebelumtidur14@gmail.com>
2024-09-10 11:35:16 +0700

    feat: test api

--- ashmawiya-be/src/index.ts
+++ ashmawiya-be/src/index.ts
@@ -1,12 +1,32 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
 
-const port = 5000;
+const prisma = new PrismaClient();
+
+app.get("/api/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json(users);
+});
+
+app.post("/api/users", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    },
+  });
+  //result and messages
+  res.json(user); 
+});
 
 app.listen(port, () => {
   console.log("server running on localhost:" + port);

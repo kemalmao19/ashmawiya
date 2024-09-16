c77c32d414596f4306380fe7f20b50748f46285a
kemalmao19 <sebelumtidur14@gmail.com>
2024-09-16 16:00:23 +0700

    fe & be: updateUserCourse & useHooks

--- ashmawiya-be/src/handlers/userCourseHandler.ts
+++ ashmawiya-be/src/handlers/userCourseHandler.ts
@@ -2,12 +2,21 @@ import { Request, Response } from "express";
 import prisma from "../config/prisma";
 import { UserCourse, Message } from "../types/response";
 
-export const userCourseByUser = async (req: Request<{ userId: number }>, res: Response<UserCourse[] | Message>) => {
-    const { userId } = req.params;
+export const userCourseAll = async (req: Request, res: Response) => {
+    try {
+        const userCourse = await prisma.userCourse.findMany()
+        res.json(userCourse)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const userCourseByUser = async (req: Request<{ id: number }>, res: Response<UserCourse[] | Message>) => {
+    const { id } = req.params;
     try {
         const userCourse = await prisma.userCourse.findMany({
             where: {
-                userId: Number(userId),
+                userId: Number(id),
             },
             include: {
                 user: {
@@ -29,5 +38,27 @@ export const userCourseByUser = async (req: Request<{ userId: number }>, res: Re
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const userCourseUpdate = async (req: Request<{ id: number }, {}, {isComplete: boolean}>, res: Response) => {
+    const {id} = req.params
+    const {isComplete} = req.body
+
+    if (!id) {
+        return res.status(400).json({ message: "ID is required" });
+    }
+    try {
+        const userCourse = await prisma.userCourse.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                isComplete
+            }
+        })
+        res.status(201).json(userCourse)
+    } catch (error) {
+        console.log(error)
     }
 }
--- ashmawiya-be/src/routes/usersCourse.ts
+++ ashmawiya-be/src/routes/usersCourse.ts
@@ -1,8 +1,13 @@
-import { Router } from "express";
-import { userCourseByUser } from "../handlers/userCourseHandler";
+import { Request, Response, Router } from "express";
+import { userCourseAll, userCourseByUser, userCourseUpdate } from "../handlers/userCourseHandler";
+import prisma from "../config/prisma";
 
 const usersCourseRouter = Router();
 
-usersCourseRouter.get("/:userId", userCourseByUser);
+usersCourseRouter.get("/", userCourseAll);
+
+usersCourseRouter.get("/user/:id", userCourseByUser);
+
+usersCourseRouter.put("/:id", userCourseUpdate);
 
 export default usersCourseRouter;

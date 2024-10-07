79f694119be812646c2a18210923d9cb0445efc9
kemalmao19 <sebelumtidur14@gmail.com>
2024-10-07 18:54:17 +0700

    feat: auth: add email validation

--- ashmawiya-fe/src/components/Auth/Login.tsx
+++ ashmawiya-fe/src/components/Auth/Login.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import {useSubmit} from "./hooks/useSubmit";
+import { useSubmit } from "./hooks/useSubmit";
 import { Layout } from "./Layout";
+import { validateEmail } from "../../lib/emailValidator";
 
 export const Login = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const {handleLogin} = useSubmit();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(true);
+
+  const { handleLogin } = useSubmit();
   return (
     <Layout>
       <h1 className="text-2xl font-semibold text-center mb-6">Login</h1>
@@ -18,13 +21,15 @@ export const Login = () => {
         <label htmlFor="email" className="text-gray-700 font-medium">
           Email
         </label>
+        {emailError ? <p className="text-red-500">Email not valid</p> : <></>}
         <input
-        id="email"
+          id="email"
           name="email"
           type="email"
           placeholder="Enter your email"
           autoComplete="on"
           required
+          onChange={(e) => setEmailError(!validateEmail(e.target.value))}
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
 
@@ -32,14 +37,13 @@ export const Login = () => {
           Password
         </label>
         <input
-        id="password"
+          id="password"
           name="password"
           type="password"
           required
           placeholder="Enter your password"
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
-
 
         <button
           type="submit"
@@ -48,7 +52,10 @@ export const Login = () => {
           {isLoading ? "Loading..." : "Login"}
         </button>
         <div>
-          Already have account? <Link to="/register" className="hover:text-black">Register</Link>
+          Already have account?{" "}
+          <Link to="/register" className="hover:text-black">
+            Register
+          </Link>
         </div>
       </form>
     </Layout>
--- ashmawiya-fe/src/components/Auth/Register.tsx
+++ ashmawiya-fe/src/components/Auth/Register.tsx
@@ -2,21 +2,28 @@ import { useState } from "react";
 import { useSubmit } from "./hooks/useSubmit";
 import { Layout } from "./Layout";
 import { Link } from "react-router-dom";
+import { validateEmail } from "../../lib/emailValidator";
 
 export const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [emailError, setEmailError] = useState<boolean>(true);
+
+  console.log(emailError);
 
   const { useRegister } = useSubmit();
-
   return (
     <Layout>
       <h1 className="text-2xl font-semibold text-center mb-6">Register</h1>
-      <form id="register" onSubmit={useRegister(setIsLoading)} className="flex flex-col gap-4">
+      <form
+        id="register"
+        onSubmit={useRegister(setIsLoading)}
+        className="flex flex-col gap-4"
+      >
         <label htmlFor="username" className="text-gray-700 font-medium">
           Username
         </label>
         <input
-        id="username"
+          id="username"
           name="username"
           type="text"
           placeholder="Enter your username"
@@ -28,13 +35,15 @@ export const Register = () => {
         <label htmlFor="email" className="text-gray-700 font-medium">
           Email
         </label>
+        {emailError ? <p className="text-red-500">Email not valid</p> : <></>}
         <input
-        id="email"
+          id="email"
           name="email"
           type="email"
           placeholder="Enter your email"
           autoComplete="on"
           required
+          onChange={(e) => setEmailError(!validateEmail(e.target.value))}
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
 
@@ -42,7 +51,7 @@ export const Register = () => {
           Password
         </label>
         <input
-        id="password"
+          id="password"
           name="password"
           type="password"
           placeholder="Enter your password"
@@ -50,14 +59,18 @@ export const Register = () => {
           className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
 
-
         <button
           type="submit"
           className={`mt-4 ${isLoading ? "cursor-not-allowed opacity-50" : ""} bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition-all duration-300`}
         >
           {isLoading ? "Loading..." : "Register"}
         </button>
-        <div>Already have account? <Link to="/login" className="hover:text-black">Login</Link></div>
+        <div>
+          Already have account?{" "}
+          <Link to="/login" className="hover:text-black">
+            Login
+          </Link>
+        </div>
       </form>
     </Layout>
   );
